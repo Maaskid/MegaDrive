d1366f43b71bb18529889e04f0816057808d14f1
MadMiata <45627230+MadMiata@users.noreply.github.com>
2021-06-26 20:17:28 +0200

    Menu fixed, Pause menu added

---
+++ MegaDrive/Assets/PauseMenuV2.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuV2 : MonoBehaviour
+{
+
+[SerializeField] private GameObject pauseMenuUI;
+[SerializeField] private bool isPaused;
+
+private void Update()
+{
+    if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        isPaused = !isPaused;
+
+    }
+
+    if(isPaused)
+    {
+        ActivateMenu();
+    }
+
+    else
+    {
+        DeactivateMenu();
+    }
+}
+
+void ActivateMenu()
+{
+    Time.timeScale = 0;
+    AudioListener.pause = true;
+    pauseMenuUI.SetActive(true);
+}
+
+public void DeactivateMenu()
+{
+    Time.timeScale = 1;
+    AudioListener.pause = false;
+    pauseMenuUI.SetActive(false);
+    isPaused = false;
+}
+
+public void ToMenuGame()
+ {
+     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+ }
+
+ public void QuitGame ()
+ {
+     Debug.Log("QUIT!");
+     Application.Quit();
+ }
+
+
+}
